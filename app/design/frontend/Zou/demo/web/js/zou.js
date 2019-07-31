ac03368dd0402b3bdb24e08b4f0122ed5eccebed
tommy <tommywhy1989@gmail.com>
2019-08-01 07:32:02 +1200

    theme create01

--- app/design/frontend/Zou/demo/web/js/zou.js
+++ app/design/frontend/Zou/demo/web/js/zou.js
@@ -1,7 +1,7 @@
 define(['jquery'], function($){
     $(function () { // to ensure that code evaluates on page load
-        // console.log('222');
-        alert("hello");
+        console.log('zou.js loaded');
+        // alert("hello");
         //这里就可以用jquery的方法了
     });
 });
--- app/etc/config.php
+++ app/etc/config.php
@@ -204,6 +204,7 @@ return [
         'Dotdigitalgroup_Email' => 1,
         'Magento_WishlistAnalytics' => 1,
         'Magento_WishlistGraphQl' => 1,
+        'Magestore_Bannerslider' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1
     ]
